525cb65449f6d4a7770608365b82e5d86e8f22e0
Edgar Duc <edgar.duc@eleves.enpc.fr>
2023-12-16 11:04:13 +0100

    Site public VF

---
+++ Alternative/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, jsonify
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/Champions_League", methods=["GET", "POST"])
+def index_CL():
+    return render_template("test.html", title="UEFA Champions League Draw", l_b="UEFA Europa League", r_b="UEFA Europa Conference League", redirect_g="index_EL", redirect_d="index_ECL", bottom="https://www.lequipe.fr/tv/")
+
+@app.route("/Europa_League", methods=["GET", "POST"])
+def index_EL():
+    return render_template("accueil.html", title="UEFA Europa League Draw", l_b="UEFA Champions League", r_b="UEFA Europa Conference League", redirect_g="index_CL", redirect_d="index_ECL", bottom="https://www.lequipe.fr/tv/")
+
+@app.route("/Europa_Conference_League", methods=["GET", "POST"])
+def index_ECL():
+    return render_template("accueil.html", title="UEFA Europa Conference League Draw", l_b="UEFA Champions League", r_b="UEFA Europa League", redirect_g="index_CL", redirect_d="index_EL", bottom="https://www.lequipe.fr/tv/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
